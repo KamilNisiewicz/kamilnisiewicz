3c568b44f46c674fdbab81728a88fc02a2f0fc88
Kamil Nisiewicz <kamnisiewicz@gmail.com>
2020-03-12 17:33:23 +0100

    Add css styles and add additional statistics

--- wyjazdy/src/components/Aways.vue
+++ wyjazdy/src/components/Aways.vue
@@ -1,35 +1,51 @@
 <template>
-  <div class="hello">
-      {{ stats.aways_count }}<br/>
-      {{ stats.distance_sum }} {{ stats.distance_avg }}<br/>
-      {{ stats.goals_scored }}-{{ stats.goals_lost}}<br/>
-      {{ stats.wins }}-{{ stats.draws }}-{{ stats.loses }}
-      <h3>Województwo śląskie</h3>
-      <ol>
-        <li v-for="away in aways_silesia" :key="away.id">
-          {{away.rival}} {{ away.attendance}}
-        </li>
-      </ol>
-      <h3>Zagranica</h3>
-      <ol>
-        <li v-for="away in aways_foreign" :key="away.id">
-          {{away.rival}} {{ away.attendance}}
-        </li>
-      </ol>
-      <h3>Polskie</h3>
-      <ol>
-        <li v-for="away in aways_poland" :key="away.id">
-          {{away.rival}} {{ away.attendance}}
-        </li>
-      </ol>
+  <div class="aways">
+    <h3>Statystyki</h3>
+    <div class="stats">
+      <div class="stats-elem">
+        <label>Liczba wyjazdów:</label>
+        <strong>{{ stats.aways_count }}</strong>
+      </div>
+      <div class="stats-elem">
+        <label>Przejechane kilometry:</label>
+        <strong>{{ stats.distance_sum }}</strong>
+      </div>
+      <div class="stats-elem">
+        <label>Średnia km na wyjazd: </label>
+        <strong>{{ stats.distance_avg }}</strong>
+      </div>
+      <div class="stats-elem">
+        <label>Zwycięstwa:</label>
+        <strong>{{ stats.wins }}</strong>
+      </div>
+      <div class="stats-elem">
+        <label>Remisy:</label>
+        <strong>{{ stats.draws }}</strong>
+      </div>
+      <div class="stats-elem">
+        <label>Porażki:</label>
+        <strong>{{ stats.loses }}</strong>
+      </div>
+      <div class="stats-elem">
+        <label>Bilans bramkowy:</label>
+        <strong>{{ stats.goals_scored }}-{{ stats.goals_lost}}</strong>
+      </div>
+    </div>
+    <List header="Województwo śląskie" :aways="aways_silesia"></List>
+    <List header="Zagranica" :aways="aways_foreign"></List>
+    <List header="Polskie" :aways="aways_poland"></List>
   </div>
 </template>
 
 <script>
 import aways from '@/assets/aways.json'
+import List from './List'
 
 export default {
   name: 'Aways',
+  components: {
+    List
+  },
   data () {
     return {
       aways: aways,
@@ -55,16 +71,26 @@ export default {
       let loses = 0
       let goals_scored = 0
       let goals_lost = 0
+      let rival_count_tmp = {}
 
       for (let i = 0; i < aways.length; i++) {
+        const current_rival = aways[i].rival
+        if(isNaN(rival_count_tmp[current_rival])){
+          rival_count_tmp[current_rival] = 0;
+        }
+        rival_count_tmp[current_rival] = (rival_count_tmp[current_rival] + 1)
+
         if (aways[i].silesia) {
           this.aways_silesia[silesia_i] = aways[i]
+          this.aways_silesia[silesia_i].rival_count = rival_count_tmp[current_rival]
           silesia_i++
         } else if (this.aways.aways[i].foreign) {
           this.aways_foreign[foreign_i] = aways[i]
+          this.aways_foreign[foreign_i].rival_count = rival_count_tmp[current_rival]
           foreign_i++
         } else {
           this.aways_poland[poland_i] = aways[i]
+          this.aways_poland[poland_i].rival_count = rival_count_tmp[current_rival]
           poland_i++
         }
 
---
+++ wyjazdy/src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="aways_list">
+        <h3>{{ header }}</h3>
+        <div class="aways_list_content aways_header">
+          <div class="elem elem-small"></div>
+          <div class="elem elem-big">Przeciwnik</div>
+          <div class="elem elem-smaller">Wynik</div>
+          <div class="elem">Data</div>
+          <div class="elem">Frekwencja</div>
+          <div class="elem">Odległość</div>
+          <div class="elem">Rozgrywki</div>
+        </div>
+        <div v-for="(away, counter) in aways" :key="away.id" class="aways_list_content aways_elem">
+          <div class="elem elem-small">{{ counter+1 }}</div>
+          <div class="elem elem-big">{{ away.rival }} ({{ away.rival_count }})</div>
+          <div class="elem elem-smaller">{{ away.score }}</div>
+          <div class="elem">{{ away.date }}</div>
+          <div class="elem">{{ away.attendance }}</div>
+          <div class="elem">{{ away.distance }}</div>
+          <div class="elem">{{ away.type }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  data () {
+    return {}
+  },
+  props: {
+    header: String,
+    aways: Object
+  }
+}
+</script>
