608780ba4035e9e724f919c8244845416b5c8a07
Kamil Nisiewicz <kamnisiewicz@gmail.com>
2020-03-12 20:06:05 +0100

    Fix ganalytics with vue plugin

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-My website
+## kamilnisiewicz.pl
+My personal website.
+TO DO at 12.03.2020
+
+## kamilnisiewicz.pl/wyjazdy
+I'm supporter of polish club - Górnik Zabrze.
+This website contains statistics about away matches of Górnik, which I watched live on stadiums.
--- wyjazdy/src/main.js
+++ wyjazdy/src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAnalytics, {
+  id: 'UA-79001704-5',
+  router
+})
 
 /* eslint-disable no-new */
 new Vue({
